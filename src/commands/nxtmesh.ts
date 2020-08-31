6614d8582aa27d80a359b84a4e7922f86cb2e0d1
Jack <29169102+Jack5079@users.noreply.github.com>
2020-08-30 23:57:15 -0400

    what am i doing

--- src/commands/nxtmesh.ts
+++ src/commands/nxtmesh.ts
@@ -1,6 +1,7 @@
-export const desc = 'spawns in a nxt logo'
-
 export function run () {
   print('not here yet, just a test')
 }
+
+export const desc = 'spawns in a nxt logo'
 export const permission = 0
+export const aliases = ['logo']
