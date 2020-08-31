388ba9d1f7ffbd6dc28ad0783a761c82fbe568ac
Jack <29169102+Jack5079@users.noreply.github.com>
2020-08-30 23:09:29 -0400

    add banland support

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,35 @@
-export = () => {
-  return 'nothing.'
-}
+import { Players } from '@rbxts/services'
 
+const banMessage = "You've been banned!"
+
+type PlayerArray = (number | string)[]
+export = ({ banland, ranks }: {
+  ranks: {
+    [key: string]: {
+      permission: number
+      // IDs/usernames of players who should have this rank.
+      people?: PlayerArray
+    }
+    Owner: {
+      permission: number,
+      people: undefined
+    }
+  }
+
+  // People who are perm-banned.
+  banland?: PlayerArray
+}) => {
+  if (banland) {
+    banland.forEach(idOrString => {
+      if (typeof idOrString === 'string') {
+        const stringPlayer = Players.GetPlayers().find(player=>player.Name === idOrString) // stupid typescript
+        if (stringPlayer) stringPlayer.Kick(banMessage)
+      } else if (typeof idOrString === 'number') {
+        const idPlayer = Players.GetPlayerByUserId(idOrString)
+        if (idPlayer) idPlayer.Kick(banMessage)
+      }
+    })
+  }
+  return 'nothing.'
+
+}
