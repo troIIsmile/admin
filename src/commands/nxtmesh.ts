df93bac0030820d5d420cf336ebdd373f0f3a436
Jack <29169102+Jack5079@users.noreply.github.com>
2020-08-31 20:01:10 -0400

    nxtmesh works!!!!!

--- src/commands/nxtmesh.ts
+++ src/commands/nxtmesh.ts
@@ -1,5 +1,17 @@
-export function run () {
-  print('not here yet, just a test')
+import { Message } from 'types'
+import { Workspace } from '@rbxts/services'
+
+export function run (message: Message) {
+  const pos = (<Model & {
+    HumanoidRootPart: Part
+  }> message.author.Character).HumanoidRootPart.Position
+  const part = new Instance('Part', Workspace)
+  part.Position = pos
+  part.BrickColor = new BrickColor('Ghost grey')
+  part.Size = new Vector3(6.057, 0.574, 3.308)
+  const mesh = new Instance('SpecialMesh', part)
+  mesh.MeshId = 'rbxassetid://5634485291'
+  mesh.Scale = new Vector3(0.9, 0.11, 1)
 }
 
 export const desc = 'spawns in a nxt logo'
