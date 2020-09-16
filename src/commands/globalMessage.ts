4d0b1548a031c9f29eea6602278b19082b7ea437
Jack <29169102+Jack5079@users.noreply.github.com>
2020-09-16 14:55:12 -0400

    globalmessage

---
+++ src/commands/globalMessage.ts
@@ -0,0 +1,32 @@
+import { MessagingService, Players, TextService } from '@rbxts/services'
+import { Message } from 'types'
+
+MessagingService.SubscribeAsync('GlobalMessageNxt', ({ Data: { message: text, from: author } }: { Data: { message: string, from: string } }) => {
+  Players.GetPlayers().forEach(plr => {
+    const gui = plr.FindFirstChild('PlayerGui') as PlayerGui | undefined
+
+    if (gui) {
+      const alertGUI = new Instance('ScreenGui', gui)
+      const label = new Instance('TextLabel', alertGUI)
+
+      label.Size = new UDim2(1, 0, 1, 0)
+      label.BackgroundColor3 = new Color3
+      label.TextColor3 = new Color3(1, 1, 1)
+      label.BackgroundTransparency = 0.5
+      label.Text = text + '\nfrom ' + author
+      wait(5)
+      alertGUI.Destroy()
+    }
+  })
+})
+
+export function run (message: Message, args: string[]) {
+  MessagingService.PublishAsync('GlobalMessageNxt', {
+    message: TextService.FilterStringAsync(args.join(' '), message.author.UserId, Enum.TextFilterContext.PublicChat),
+    from: message.author.Name
+  })
+}
+
+export const desc = 'Display a message on every server.'
+export const permission = 3
+export const aliases = ['ga', 'gm']
