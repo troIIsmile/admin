be63bb55ef02bffc98e87fac303d02b519be612d
Jack <29169102+Jack5079@users.noreply.github.com>
2020-10-24 19:56:57 -0400

    ...

--- src/commands/cmds.ts
+++ src/commands/cmds.ts
@@ -3,6 +3,7 @@ import { Message, Bot } from 'types'
 
 export const desc = 'A list of commands.'
 export const permission = 0
+export const aliases = ['help', 'commands']
 export function run (_: Message, __: string[], bot: Bot, permission: number) {
   return [...bot.commands.entries()].filter(([, { permission: cmdpermission }]) => permission >= cmdpermission).map(([name, { desc }]) => {
     return `${name} - ${desc}`
