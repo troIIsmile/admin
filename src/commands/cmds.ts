84fd03613739b1626b870347693c7e8bce4cc5ab
Jack <29169102+Jack5079@users.noreply.github.com>
2020-10-24 19:56:22 -0400

    add cmds

---
+++ src/commands/cmds.ts
@@ -0,0 +1,10 @@
+
+import { Message, Bot } from 'types'
+
+export const desc = 'A list of commands.'
+export const permission = 0
+export function run (_: Message, __: string[], bot: Bot, permission: number) {
+  return [...bot.commands.entries()].filter(([, { permission: cmdpermission }]) => permission >= cmdpermission).map(([name, { desc }]) => {
+    return `${name} - ${desc}`
+  }).join('\n')
+}
