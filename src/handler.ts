5e675a5cb496ec2d9893403a1085bf5b83affb00
Jack <29169102+Jack5079@users.noreply.github.com>
2020-10-24 20:06:16 -0400

    h

--- src/handler.ts
+++ src/handler.ts
@@ -1,5 +1,5 @@
 import notifEv from 'notify'
-import { Bot } from 'types'
+import { Bot, Message } from 'types'
 import printEv from 'print'
 
 declare const script: ModuleScript & {
@@ -11,7 +11,7 @@ declare const script: ModuleScript & {
 }
 
 export = async (bot: Bot, prefix: string, plr: Player, content: string, sound: number, to?: Player) => {
-  const message = {
+  const message: Message = {
     author: plr,
     channel: to,
     content
@@ -21,40 +21,39 @@ export = async (bot: Bot, prefix: string, plr: Player, content: string, sound: n
       content.startsWith(`${prefix}${cmdname} `) || // matches any command with a space after
       content === prefix + cmdname // matches any command without arguments
   )
+  if (!name) return
   // Run the command!
-  if (name) {
-    const permissionOfPlayer = (bot.ranks.get(bot.rankOf.get(plr) as string) as { permission: number }).permission
-    const command = (bot.commands.get(name) || { run: undefined }).run // The command if it found it
-      || (bot.commands.get(bot.aliases.get(name) || '') || { run: undefined }).run // Aliases
-      || (() => {}) // nothing
+  const permissionOfPlayer = (bot.ranks.get(bot.rankOf.get(plr) as string) as { permission: number }).permission
+  const command = (bot.commands.get(name) || { run: undefined }).run // The command if it found it
+    || (bot.commands.get(bot.aliases.get(name) || '') || { run: undefined }).run // Aliases
+    || (() => { }) // nothing
 
-    const permission = (bot.commands.get(name) || { permission: undefined }).permission // The command if it found it
-      || (bot.commands.get(bot.aliases.get(name) || '') || { permission: undefined }).permission // Aliases
-      || 0 // nothing
-    if (permissionOfPlayer >= permission) {
-      const output = await command(
-        message, // the message
-        // The arguments
-        content
-          .sub(prefix.size() + 1 + name.size()) // only the part after the command
-          .split(' '), // split with spaces
-        bot, // give em the bot
-        permissionOfPlayer // give em the permission
-      )
+  const permission = (bot.commands.get(name) || { permission: undefined }).permission // The command if it found it
+    || (bot.commands.get(bot.aliases.get(name) || '') || { permission: undefined }).permission // Aliases
+    || 0 // nothing
+  if (permissionOfPlayer >= permission) {
+    const output = await command(
+      message, // the message
+      // The arguments
+      content
+        .sub(prefix.size() + 1 + name.size()) // only the part after the command
+        .split(' '), // split with spaces
+      bot, // give em the bot
+      permissionOfPlayer // give em the permission
+    )
 
-      if (output) {
-        wait()
-        printEv.FireClient(plr, {
-          Text: output,
-          Font: Enum.Font.RobotoMono
-        })
-      }
-    } else {
-      notifEv.FireClient(plr, {
-        Title: 'trollsmile',
-        Button1: 'Close',
-        Text: "You do not have the permission to run this command."
-      }, sound)
+    if (output) {
+      wait()
+      printEv.FireClient(plr, {
+        Text: output,
+        Font: Enum.Font.RobotoMono
+      })
     }
+  } else {
+    notifEv.FireClient(plr, {
+      Title: 'trollsmile',
+      Button1: 'Close',
+      Text: "You do not have the permission to run this command."
+    }, sound)
   }
 }
