2a21b34a3b1283fd17fcda9c7878796ef2469644
Jack <29169102+Jack5079@users.noreply.github.com>
2020-10-29 15:29:21 -0400

    remove braces

--- src/utils.ts
+++ src/utils.ts
@@ -4,9 +4,7 @@ import { Message } from 'types'
 
 const getPlayersNoComma = (String = 'N/A') => String === 'all' ? Players.GetPlayers() : Players.GetPlayers().filter(plr => !!plr.Name.lower().match('^' + String.lower())[0])
 export const getPlayers = (String = 'N/A'): Player[] => removeDuplicates(([] as Player[]).concat(...String.split(',').map(getPlayersNoComma)))
-export const removeDuplicates = <Type> (array: Type[]): Type[] => {
-  return [...new Set(array)]
-}
+export const removeDuplicates = <Type> (array: Type[]): Type[] => [...new Set(array)]
 export function plrCommand (command: (plr: Player) => unknown) {
   return (message: Message, args: string[]) => {
     if (args.join('').trim().size()) {
