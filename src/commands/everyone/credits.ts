2a6750e80cd7c158b28dfdb1569a88082c964464
Jack <29169102+Jack5079@users.noreply.github.com>
2020-11-06 16:18:55 -0500

    :D

---
+++ src/commands/everyone/credits.ts
@@ -0,0 +1,11 @@
+import { Players } from '@rbxts/services'
+import { Message, Bot } from 'types'
+
+export const desc = 'trollsmile credits'
+export const permission = 0
+export function run () {
+  return `
+  trollsmile admin by Jack
+  ${Players.GetNameFromUserIdAsync(458316463)} is nice :D
+  `.split('\n').map(line=>line.trim()).join('\n').trim()
+}
