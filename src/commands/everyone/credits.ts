f99208c1f781393a289318c1c1937071b495c86a
Jack <29169102+Jack5079@users.noreply.github.com>
2020-11-09 14:24:50 -0500

    de frig

--- src/commands/everyone/credits.ts
+++ src/commands/everyone/credits.ts
@@ -1,9 +1,9 @@
 export const desc = 'trollsmile credits'
 export const permission = 0
-export function run () {
-  return `
-  trollsmile-admin by Jack
-  powered by roblox-ts
-  command handler from trollsmile-djs by Jack
-  `.split('\n').map(line=>line.trim()).join('\n').trim()
-}
+export const run = () => `
+
+trollsmile-admin by Jack
+powered by roblox-ts
+command handler from trollsmile-djs by Jack
+
+`.split('\n').map(line => line.trim()).filter(a=>!!a).join('\n').trim()
