cfdd67aa3519fd6e5a91f6d110a83381a07b9ef2
Jack <29169102+Jack5079@users.noreply.github.com>
2020-11-09 14:23:26 -0500

    D:

--- src/commands/everyone/credits.ts
+++ src/commands/everyone/credits.ts
@@ -1,10 +1,9 @@
-import { Players } from '@rbxts/services'
-
 export const desc = 'trollsmile credits'
 export const permission = 0
 export function run () {
   return `
-  trollsmile admin by Jack
-  ${Players.GetNameFromUserIdAsync(458316463)} is nice :D
+  trollsmile-admin by Jack
+  powered by roblox-ts
+  command handler from trollsmile-djs by Jack
   `.split('\n').map(line=>line.trim()).join('\n').trim()
 }
