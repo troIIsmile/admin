b107525da748fdcba124dd7a1dd47aadcecde93b
Jack <29169102+Jack5079@users.noreply.github.com>
2020-11-09 18:49:38 -0500

    Revert "D:"

--- src/commands/everyone/credits.ts
+++ src/commands/everyone/credits.ts
@@ -4,6 +4,7 @@ export const run = () => `
 
 trollsmile-admin by Jack
 powered by roblox-ts
+${Players.GetNameFromUserIdAsync(458316463)} is nice :D
 command handler from trollsmile-djs by Jack
 
 `.split('\n').map(line => line.trim()).filter(a=>!!a.size()).join('\n').trim()
