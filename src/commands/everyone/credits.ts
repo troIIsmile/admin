32176da1c7ac322602c86e5990e069b85487ddfb
Jack <29169102+Jack5079@users.noreply.github.com>
2020-11-09 14:25:02 -0500

    fix

--- src/commands/everyone/credits.ts
+++ src/commands/everyone/credits.ts
@@ -6,4 +6,4 @@ trollsmile-admin by Jack
 powered by roblox-ts
 command handler from trollsmile-djs by Jack
 
-`.split('\n').map(line => line.trim()).filter(a=>!!a).join('\n').trim()
+`.split('\n').map(line => line.trim()).filter(a=>!!a.size()).join('\n').trim()
