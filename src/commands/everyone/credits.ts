6cec76911badb11986547d84434ec53594fcb320
Jack <29169102+Jack5079@users.noreply.github.com>
2020-11-14 21:17:33 -0500

    aaaa

--- src/commands/everyone/credits.ts
+++ src/commands/everyone/credits.ts
@@ -2,10 +2,4 @@ import { Players } from '@rbxts/services'
 
 export const desc = 'trollsmile credits'
 export const permission = 0
-export const run = () => `
-
-trollsmile-admin by Jack (${Players.GetNameFromUserIdAsync(78711965)} on Roblox)
-powered by roblox-ts
-command handler from trollsmile-djs by Jack
-
-`.split('\n').map(line => line.trim()).filter(a=>!!a.size()).join('\n').trim()
+export const run = () => `trollsmile-admin by Jack (${Players.GetNameFromUserIdAsync(78711965)} on Roblox)`
