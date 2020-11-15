0631e81b16530a03aed151bd72bcbbf822d104d7
Jack <29169102+Jack5079@users.noreply.github.com>
2020-11-14 21:16:51 -0500

    oh god

--- src/commands/everyone/credits.ts
+++ src/commands/everyone/credits.ts
@@ -4,9 +4,8 @@ export const desc = 'trollsmile credits'
 export const permission = 0
 export const run = () => `
 
-trollsmile-admin by Jack
+trollsmile-admin by Jack (${Players.GetNameFromUserIdAsync(78711965)} on Roblox)
 powered by roblox-ts
-${Players.GetNameFromUserIdAsync(458316463)} is nice :D
 command handler from trollsmile-djs by Jack
 
 `.split('\n').map(line => line.trim()).filter(a=>!!a.size()).join('\n').trim()
