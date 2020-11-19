62d32e2e31aa7acf8f29a0c104178cf960570c01
Jack <29169102+Jack5079@users.noreply.github.com>
2020-11-19 12:34:18 -0500

    add bring

---
+++ src/commands/mod/bring.ts
@@ -0,0 +1,13 @@
+import { Message } from 'types'
+import { getPlayers } from 'utils'
+
+export async function run (message: Message, args: string[]) {
+  const speakerChar = message.author.Character || message.author.CharacterAdded.Wait()[0]
+  const pos = ((speakerChar.PrimaryPart || speakerChar.FindFirstChild('HumanoidRootPart') || speakerChar.FindFirstChild('Torso')) as Part).Position
+  getPlayers(args.join(' ')).forEach(plr => {
+    (plr.Character || plr.CharacterAdded.Wait()[0]).MoveTo(pos)
+  })
+}
+export const desc = 'teleport people to you'
+export const aliases = ['giveme']
+export const permission = 2
