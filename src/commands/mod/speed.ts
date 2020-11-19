6568fbde0b083e254ee3363097b7beba62889111
Jack <29169102+Jack5079@users.noreply.github.com>
2020-11-19 12:46:40 -0500

    i am so stupid

--- src/commands/mod/speed.ts
+++ src/commands/mod/speed.ts
@@ -1,10 +1,17 @@
 import { StarterPlayer } from '@rbxts/services'
 import { Message } from 'types'
+import { getPlayers } from 'utils'
 
-export async function run (message: Message, args: string[]) {
-  const { author: plr } = message
-  const hum = (plr.Character || plr.CharacterAdded.Wait()[0]).FindFirstAncestorWhichIsA('Humanoid')
-  if (hum) hum.WalkSpeed = tonumber(args.join('')) || StarterPlayer.CharacterWalkSpeed
+export async function run (message: Message, [speedOrPlrs, speed]: string[]) {
+  if (!tonumber(speedOrPlrs)) {
+    getPlayers(speedOrPlrs).forEach(plr => {
+      const hum = (plr.Character || plr.CharacterAdded.Wait()[0]).FindFirstChildWhichIsA('Humanoid')
+      if (hum) hum.WalkSpeed = tonumber(speed) || StarterPlayer.CharacterWalkSpeed
+    })
+  } else {
+    const hum = (message.author.Character || message.author.CharacterAdded.Wait()[0]).FindFirstChildWhichIsA('Humanoid')
+    if (hum) hum.WalkSpeed = tonumber(speedOrPlrs)  || StarterPlayer.CharacterWalkSpeed
+  }
 }
 export const desc = 'gotta go fast'
 export const aliases = ['walkspeed', 'walkSpeed']
