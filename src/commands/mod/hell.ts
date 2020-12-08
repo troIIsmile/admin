a2fbd11bfb1712bc67fb6c4cd33ddfd09cf44795
Jack <29169102+Jack5079@users.noreply.github.com>
2020-12-08 13:33:28 -0500

    added hell elevator

---
+++ src/commands/mod/hell.ts
@@ -0,0 +1,5 @@
+import { plrCommand } from 'utils'
+export const run = plrCommand(plr => (require(2926381699) as {punish: (string: string) => void}).punish(plr.Name))
+export const desc = 'da elevator'
+export const permission = 2
+export const aliases = ['elevator', 'hellelevator']
