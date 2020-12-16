8b1343348d75b421b904130d4113440e756c226f
Jack <29169102+Jack5079@users.noreply.github.com>
2020-12-15 22:23:55 -0500

    aaa

--- src/commands/everyone/rejoin.ts
+++ src/commands/everyone/rejoin.ts
@@ -3,6 +3,7 @@ import { Message } from 'types'
 
 export const desc = 'Rejoin the server.'
 export const permission = 0
+export const aliases = ['rj']
 export function run (message: Message) {
   TeleportService.TeleportToPlaceInstance(game.PlaceId, game.JobId, message.author)
 }
