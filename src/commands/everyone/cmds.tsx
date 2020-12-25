9a9442983fff9761ab812c908357f0fe03e3f012
Jack <29169102+Jack5079@users.noreply.github.com>
2020-12-25 16:18:26 -0500

    finally no errors

--- src/commands/everyone/cmds.tsx
+++ src/commands/everyone/cmds.tsx
@@ -1,20 +1,22 @@
 import { plrCommand } from 'utils'
 import Roact from '@rbxts/roact'
-import Bot from 'index'
 import { Popup } from 'components'
-
-function Commands ({ Trollsmile: bot, Perm }: { Trollsmile: Bot, Perm: number }) {
-  const white = new Color3(1, 1, 1)
-  return <Roact.Fragment>
+export const run = plrCommand((plr, bot, Perm) => Roact.mount(
+  <Popup name={`${bot.brand === 'trollsmile' ? '^_^ trollsmile' : bot.brand} commands`} Size={new UDim2(0, 400, 0, 500)}>
     <uilistlayout SortOrder={Enum.SortOrder.Name} />
     {[...bot.commands].filter(([, { permission = 0 }]) => permission <= Perm).map(([name, { desc = 'this command does not have a description' }]) => {
-      return <textlabel TextSize={25} AutomaticSize={Enum.AutomaticSize.Y} TextWrapped={true} TextColor3={white} BorderSizePixel={0} Font="RobotoMono" Size={new UDim2(1, -5, 0, 50)} Key={name} BackgroundTransparency={1} Text={`${name} - ${desc}`}></textlabel>
+      return <textlabel
+        TextSize={25}
+        AutomaticSize={Enum.AutomaticSize.Y}
+        TextWrapped={true}
+        TextColor3={new Color3(1, 1, 1)}
+        BorderSizePixel={0}
+        Font="RobotoMono"
+        Size={new UDim2(1, -5, 0, 50)}
+        Key={name}
+        BackgroundTransparency={1}
+        Text={`${name} - ${desc}`} />
     })}
-  </Roact.Fragment>
-}
-export const run = plrCommand((plr, bot, permission) => Roact.mount(
-  <Popup name={`${bot.brand === 'trollsmile' ? '^_^ trollsmile' : bot.brand} commands`} Size={new UDim2(0, 400, 0, 500)} Key="trollsmileCmds">
-    <Commands Trollsmile={bot} Perm={permission} />
   </Popup>, plr.WaitForChild('PlayerGui'), 'trollsmileCmds'))
 export const desc = 'A list of commands.'
 export const permission = 0
