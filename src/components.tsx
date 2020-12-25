27bbc5024d449378e2c56714256c131be7330136
Jack <29169102+Jack5079@users.noreply.github.com>
2020-12-25 16:40:14 -0500

    please work

--- src/components.tsx
+++ src/components.tsx
@@ -1,8 +1,8 @@
 import Roact from '@rbxts/roact'
-export function Center (props: { [Roact.Children]: Roact.Element[] | Roact.Element }) {
+export function Center ({ [Roact.Children]: child }: { [Roact.Children]: Roact.Element }) {
   return <frame BackgroundTransparency={1} BorderSizePixel={0} Key="center" Size={new UDim2(1, 0, 1, 0)}>
     <uilistlayout HorizontalAlignment={Enum.HorizontalAlignment.Center} VerticalAlignment={Enum.VerticalAlignment.Center} />
-    {props[Roact.Children]}
+    {child}
   </frame>
 }
 
