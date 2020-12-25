5d7ec2f7441de012f8e85d126047643cbe40e316
Jack <29169102+Jack5079@users.noreply.github.com>
2020-12-24 20:50:05 -0500

    fix speed i think

--- src/commands/mod/speed.ts
+++ src/commands/mod/speed.ts
@@ -2,16 +2,16 @@ import { StarterPlayer } from '@rbxts/services'
 import { Message } from 'types'
 import { getPlayers } from 'utils'
 
-export async function run (message: Message, [speedOrPlrs, speed]: string[]) {
-  if (!tonumber(speedOrPlrs)) {
-    getPlayers(speedOrPlrs).forEach(plr => {
-      const hum = (plr.Character || plr.CharacterAdded.Wait()[0]).FindFirstChildWhichIsA('Humanoid')
-      if (hum) hum.WalkSpeed = tonumber(speed) || StarterPlayer.CharacterWalkSpeed
-    })
-  } else {
-    const hum = (message.author.Character || message.author.CharacterAdded.Wait()[0]).FindFirstChildWhichIsA('Humanoid')
-    if (hum) hum.WalkSpeed = tonumber(speedOrPlrs)  || StarterPlayer.CharacterWalkSpeed
-  }
+export async function run (message: Message, args: string[]) {
+  const speed = tonumber(args.find(string => tonumber(string) !== undefined)) || StarterPlayer.CharacterWalkSpeed
+  const players = getPlayers(args.filter(string => tonumber(string) === undefined).join(' '))
+  players.forEach(plr => {
+    const hum = plr.Character?.FindFirstChildWhichIsA('Humanoid')
+    if (hum) {
+      hum.WalkSpeed = speed
+    }
+  })
+  
 }
 export const desc = 'gotta go fast'
 export const aliases = ['walkspeed', 'walkSpeed']
