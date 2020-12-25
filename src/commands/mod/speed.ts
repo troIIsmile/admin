16109c324ade9812c5cf5ef1796851f0c2d2e0ed
Jack <29169102+Jack5079@users.noreply.github.com>
2020-12-24 20:54:01 -0500

    make speed work without a list of players

--- src/commands/mod/speed.ts
+++ src/commands/mod/speed.ts
@@ -5,13 +5,20 @@ import { getPlayers } from 'utils'
 export async function run (message: Message, args: string[]) {
   const speed = tonumber(args.find(string => tonumber(string) !== undefined)) || StarterPlayer.CharacterWalkSpeed
   const players = getPlayers(args.filter(string => tonumber(string) === undefined).join(' '))
-  players.forEach(plr => {
-    const hum = plr.Character?.FindFirstChildWhichIsA('Humanoid')
+  if (players.size() === 0) {
+    const hum = message.author.Character?.FindFirstChildWhichIsA('Humanoid')
     if (hum) {
       hum.WalkSpeed = speed
     }
-  })
-  
+  } else {
+    players.forEach(plr => {
+      const hum = plr.Character?.FindFirstChildWhichIsA('Humanoid')
+      if (hum) {
+        hum.WalkSpeed = speed
+      }
+    })
+  }
+
 }
 export const desc = 'gotta go fast'
 export const aliases = ['walkspeed', 'walkSpeed']
