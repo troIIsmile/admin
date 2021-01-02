4986b62dd1509fe205446a06c6bb50899035882d
Jack <29169102+Jack5079@users.noreply.github.com>
2021-01-02 11:57:18 -0500

    remove remove dupes

--- src/utils.ts
+++ src/utils.ts
@@ -12,8 +12,6 @@ export const flatten = <Type> (arr: Type[][]): Type[] => {
   return newarr
 }
 export const getPlayers = (String = 'N/A') => StringUtils.trim(String) === 'all' ? Players.GetPlayers() : Players.GetPlayers().filter(plr => !!plr.Name.lower().match('^' + StringUtils.trim(String.lower()))[0])
-
-export const removeDuplicates = <Type> (array: Type[]): Type[] => [...new Set(array)]
 export function plrCommand (command: (plr: Player, bot: Bot, permission: number) => unknown) {
   return (message: Message, args: string[], bot: Bot, perm: number) => {
     if (StringUtils.trim(args.join('')).size()) {
