c9374084b3958c90f26b40c8cddd6c0024c67ce0
Jack <29169102+Jack5079@users.noreply.github.com>
2021-01-03 12:37:00 -0500

    add PKG_VERSION to credits

--- src/commands/everyone/credits.ts
+++ src/commands/everyone/credits.ts
@@ -2,4 +2,4 @@ import { Players } from '@rbxts/services'
 
 export const desc = 'trollsmile credits'
 export const permission = 0
-export const run = () => `trollsmile-admin by Jack (${Players.GetNameFromUserIdAsync(78711965)} on Roblox)`
+export const run = () => `trollsmile-admin v${PKG_VERSION} by Jack (${Players.GetNameFromUserIdAsync(78711965)} on Roblox\npowered by roblox-ts, Roact, `
