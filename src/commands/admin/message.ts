8666ed43dbc1cc1bd2f19dd7bb6c4d4fad22d604
Jack <29169102+Jack5079@users.noreply.github.com>
2021-01-17 18:21:39 -0500

    add way better message

--- src/commands/admin/message.ts
+++ src/commands/admin/message.ts
@@ -1,4 +1,4 @@
-import { Chat, Players } from '@rbxts/services'
+import { Chat, Players, TweenService, Debris } from '@rbxts/services'
 import { Message } from 'types'
 
 export function run (message: Message, args: string[]) {
@@ -8,15 +8,32 @@ export function run (message: Message, args: string[]) {
 
     if (gui) {
       const alertGUI = new Instance('ScreenGui', gui)
+      alertGUI.IgnoreGuiInset = true
+      Debris.AddItem(alertGUI, 5 + text.size() / 30)
       const label = new Instance('TextLabel', alertGUI)
-
+      label.TextTransparency = 1
+      label.BackgroundTransparency = 1
+      label.Position = new UDim2(0, 0, -0.5, 0)
+      TweenService.Create(label, new TweenInfo(1, Enum.EasingStyle.Exponential), {
+        Position: new UDim2(0, 0, 0, 0),
+        TextTransparency: 0,
+        BackgroundTransparency: 0.5
+      }).Play()
+      label.Text = text + '\nfrom ' + message.author.Name
       label.Size = new UDim2(1, 0, 1, 0)
+      label.TextSize = 20
+      label.Font = Enum.Font.RobotoMono
       label.BackgroundColor3 = new Color3
       label.TextColor3 = new Color3(1, 1, 1)
+      wait(1)
+      label.Position = new UDim2(0, 0, 0, 0)
       label.BackgroundTransparency = 0.5
-      label.Text = text + '\nfrom ' + message.author.Name
-      wait(3 + text.size() / 30) // https://github.com/1ForeverHD/HDAdminV2/blob/61d8a9a960319bc27b53b25494048ae22892b014/MainModule/Client/SharedModules/SharedCoreFunctions.lua#L334
-      alertGUI.Destroy()
+      wait(1)
+      TweenService.Create(label, new TweenInfo(1, Enum.EasingStyle.Exponential), {
+        Position: new UDim2(0, 0, 0.5, 0),
+        TextTransparency: 1,
+        BackgroundTransparency: 1
+      }).Play()
     }
   })
 }
