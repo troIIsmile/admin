dcefd248517dc16286750c6081d4066adb491662
Jack <29169102+Jack5079@users.noreply.github.com>
2021-01-27 14:36:14 -0500

    trollar

--- src/commands/everyone/cmds.tsx
+++ src/commands/everyone/cmds.tsx
@@ -1,24 +1,27 @@
-import { plrCommand } from 'utils'
 import Roact from '@rbxts/roact'
 import { Popup } from 'components'
 import { HttpService } from '@rbxts/services'
-export const run = plrCommand((plr, bot, Perm) => Roact.mount(
-  <Popup name={`${bot.brand === 'trollsmile' ? '^_^ trollsmile' : bot.brand} commands`} Size={new UDim2(0, 400, 0, 500)}>
-    <uilistlayout SortOrder={Enum.SortOrder.Name} />
-    {[...bot.commands].filter(([, { permission = 0 }]) => permission <= Perm).map(([name, { desc = 'this command does not have a description' }]) => {
-      return <textlabel
-        TextSize={25}
-        AutomaticSize={Enum.AutomaticSize.Y}
-        TextWrapped={true}
-        TextColor3={new Color3(1, 1, 1)}
-        BorderSizePixel={0}
-        Font="RobotoMono"
-        Size={new UDim2(1, -5, 0, 50)}
-        Key={name}
-        BackgroundTransparency={1}
-        Text={`${name} - ${desc}`} />
-    })}
-  </Popup>, plr.WaitForChild('PlayerGui'), HttpService.GenerateGUID(false)))
+import { Message } from 'types'
+import Trollsmile from 'index'
+export function run ({ author: plr }: Message, args: string[], bot: Trollsmile, Perm: number) {
+  Roact.mount(
+    <Popup name={`${bot.brand === 'trollsmile' ? '^_^ trollsmile' : bot.brand} commands`} Size={new UDim2(0, 400, 0, 500)}>
+      <uilistlayout SortOrder={Enum.SortOrder.Name} />
+      {[...bot.commands].filter(([, { permission = 0 }]) => permission <= Perm).map(([name, { desc = 'this command does not have a description' }]) => {
+        return <textlabel
+          TextSize={25}
+          AutomaticSize={Enum.AutomaticSize.Y}
+          TextWrapped={true}
+          TextColor3={new Color3(1, 1, 1)}
+          BorderSizePixel={0}
+          Font="RobotoMono"
+          Size={new UDim2(1, -5, 0, 50)}
+          Key={name}
+          BackgroundTransparency={1}
+          Text={`${name} - ${desc}`} />
+      })}
+    </Popup>, plr.WaitForChild('PlayerGui'), HttpService.GenerateGUID(false))
+}
 export const desc = 'A list of commands.'
 export const permission = 0
 export const aliases = ['help', 'list']
