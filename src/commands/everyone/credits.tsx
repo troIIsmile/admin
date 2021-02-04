8f86a46c195a9b23932d9b12e66afe41d3b2fd92
Jack <29169102+Jack5079@users.noreply.github.com>
2021-02-04 13:36:29 -0500

    add trollsmile winning easter egg to credits click luaquack's avatar

--- src/commands/everyone/credits.tsx
+++ src/commands/everyone/credits.tsx
@@ -1,4 +1,4 @@
-import { Players, HttpService } from '@rbxts/services'
+import { Players, HttpService, SoundService } from '@rbxts/services'
 import { Popup } from 'components'
 import Roact from '@rbxts/roact'
 import Trollsmile from 'index'
@@ -8,14 +8,22 @@ import { AutoResize, random } from 'utils'
 export const desc = 'trollsmile credits'
 export const permission = 0
 export const aliases = ['about', 'winning', 'trollsmile', 'meta']
-function Credit ({ id, they }: { id: number, they?: string }) {
+function Credit ({ id, they, onclick }: { id: number, they?: string, onclick?: (rbx: ImageButton) => unknown }) {
   return <frame BorderSizePixel={0} BackgroundTransparency={1} Size={new UDim2(1, 0, 0, 50)}>
     <uilistlayout FillDirection="Horizontal" />
-    <imagelabel
+    {onclick ? <imagebutton
       BorderSizePixel={0}
       Image={Players.GetUserThumbnailAsync(id, Enum.ThumbnailType.AvatarBust, Enum.ThumbnailSize.Size420x420)[0]}
       Size={new UDim2(0, 50, 0, 50)}
-    />
+      AutoButtonColor={false}
+      Event={{
+        MouseButton1Click: onclick
+      }}
+    /> : <imagelabel
+      BorderSizePixel={0}
+      Image={Players.GetUserThumbnailAsync(id, Enum.ThumbnailType.AvatarBust, Enum.ThumbnailSize.Size420x420)[0]}
+      Size={new UDim2(0, 50, 0, 50)}
+    />}
     {they ? (<frame BorderSizePixel={0} BackgroundTransparency={1} Size={new UDim2(0, 345, 0, 50)}>
       <uilistlayout FillDirection="Vertical" />
       <textlabel Text={Players.GetNameFromUserIdAsync(id)} Size={new UDim2(1, 0, .5, 0)} BorderSizePixel={0} BackgroundTransparency={1} Font="Roboto" TextScaled={true} TextColor3={new Color3(1, 1, 1)}>
@@ -36,7 +44,7 @@ export function run ({ author: plr }: Message, args: string[], bot: Trollsmile) 
       {Roact.createElement('Sound', {
         Playing: true,
         Looped: true,
-        Volume: 10,
+        Volume: 5,
         SoundId: 'rbxassetid://' + random([
           6101736348, // that one luaquack script
           1571897668, // Conro - On My Way Up
@@ -58,7 +66,13 @@ export function run ({ author: plr }: Message, args: string[], bot: Trollsmile) 
       <textlabel Text={"Special thanks to"} Size={new UDim2(1, 0, 0, 50)} BorderSizePixel={0} BackgroundTransparency={1} Font="Roboto" TextScaled={true} TextColor3={new Color3(1, 1, 1)} TextXAlignment="Center">
         <uitextsizeconstraint MaxTextSize={25} />
       </textlabel>
-      <Credit id={1308783007} they="He's LuaQuack" />
+      <Credit id={1308783007} they="He's LuaQuack" onclick={(rbx) => {
+        const sound = new Instance('Sound', rbx)
+        sound.Volume = 10
+        sound.SoundId = 'rbxassetid://6345755361'
+        sound.PlayOnRemove = true
+        sound.Destroy()
+      }} />
       <textlabel Text={"Uses libraries developed by"} Size={new UDim2(1, 0, 0, 50)} BorderSizePixel={0} BackgroundTransparency={1} Font="Roboto" TextScaled={true} TextColor3={new Color3(1, 1, 1)} TextXAlignment="Center">
         <uitextsizeconstraint MaxTextSize={25} />
       </textlabel>
