59723076b92a689d6e50a05ec3bec51b200dbae7
Jack <29169102+Jack5079@users.noreply.github.com>
2021-02-04 17:39:14 -0500

    add friends to getPlayers, better timing for message, blind uses Roact

---
+++ src/commands/mod/blind.tsx
@@ -0,0 +1,10 @@
+import Roact from '@rbxts/roact'
+import { plrCommand } from 'utils'
+export const run = plrCommand(plr => {
+  Roact.mount(<screengui ResetOnSpawn={false} DisplayOrder={2147483647} IgnoreGuiInset>
+    <frame BorderSizePixel={0} BackgroundColor3={new Color3} Size={new UDim2(1, 0, 1, 0)} />
+  </screengui>, plr.FindFirstChildWhichIsA('PlayerGui'), 'trollsmileBlind')
+})
+export const desc = "aaa i can't see"
+export const permission = 2
+export const aliases = []
