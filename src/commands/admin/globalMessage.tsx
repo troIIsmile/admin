2c7c8d75f0b388148dea0075c5d3d68bb38c28c2
Jack <git@5079.ml>
2021-02-26 15:23:38 -0500

    Convert message GUI to a component Fixes #8

--- src/commands/admin/globalMessage.tsx
+++ src/commands/admin/globalMessage.tsx
@@ -1,20 +1,14 @@
-import { Chat, Debris, MessagingService, Players } from '@rbxts/services'
+import { Chat, MessagingService, Players } from '@rbxts/services'
 import { message } from 'types'
+import { Message } from 'components'
+import Roact from '@rbxts/roact'
 
 MessagingService.SubscribeAsync('GlobalMessageTrollsmile', ({ Data: [text, author] }: { Data: [string, string] }) => {
   Players.GetPlayers().forEach(plr => {
     const gui = plr.FindFirstChildWhichIsA('PlayerGui')
 
     if (gui) {
-      const alertGUI = new Instance('ScreenGui', gui)
-      const label = new Instance('TextLabel', alertGUI)
-
-      label.Size = new UDim2(1, 0, 1, 0)
-      label.BackgroundColor3 = new Color3
-      label.TextColor3 = new Color3(1, 1, 1)
-      label.BackgroundTransparency = 0.5
-      label.Text = text + '\nfrom ' + author
-      Debris.AddItem(alertGUI, 3 + text.size() / 30) // https://github.com/1ForeverHD/HDAdminV2/blob/61d8a9a960319bc27b53b25494048ae22892b014/MainModule/Client/SharedModules/SharedCoreFunctions.lua#L334
+      Roact.mount(<Message author={author} text={text} />, gui)
     }
   })
 })
